db130f029c2e0978fdbd8e0fbc0eb1084cf619e2
Andriy Salnikov <a.salnikov.home@gmail.com>
2023-08-11 10:17:14 +0300

    Add README.md

--- src/main/java/ua/pp/salnikov/pizzatesttask/repository/OrderRepository.java
+++ src/main/java/ua/pp/salnikov/pizzatesttask/repository/OrderRepository.java
@@ -2,11 +2,13 @@ package ua.pp.salnikov.pizzatesttask.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ua.pp.salnikov.pizzatesttask.model.Order;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     @Query(value = "SELECT * FROM orders WHERE status = 'ACTIVE'", nativeQuery = true)
